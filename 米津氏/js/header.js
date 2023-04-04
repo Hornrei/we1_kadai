cc8c3385e1857ea7fe756e42e9831e475268e744
hornrei <hornrei0808@gmail.com>
2023-04-04 23:45:19 +0900

    課題更新

---
+++ 米津氏/js/header.js
@@ -0,0 +1,9 @@
+
+'use strict'; 
+ 
+jQuery(window).on('scroll', function () {
+ 
+ if (jQuery('.header').height() < jQuery(this).scrollTop()) { 
+jQuery('.header').addClass('change-color'); }
+ else {
+ jQuery('.header').removeClass('change-color'); } });
